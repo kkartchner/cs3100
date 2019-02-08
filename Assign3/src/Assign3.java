07156f9ffec989d9a26d05e355fe90ed15d2b760
Ky Kartchner <kartchnerky@gmail.com>
2019-02-07 20:25:33 -0700

    updates to Assign3

--- Assign3/src/Assign3.java
+++ Assign3/src/Assign3.java
@@ -1,112 +1,165 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.lang.Runtime;
-import java.util.Properties;
 import java.util.Scanner;
 
 public class Assign3 {
+    private static ArrayList<String> commandHistory = new ArrayList<>();
+
     public static void main(String[] args) {
-        // Test if args[0] is a built in shell command
-        //   If so, perform that operation
-        //   Else, attempt to execute it as external program
-        
-        Scanner sc = new Scanner(System.in);
-        Properties properties = Runtime.getProperties();
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.printf("[%s]: ", System.getProperty("user.dir"));
 
-        while (true){
-            prompt();
-
-            String command = sc.next();
-            System.out.println(command);
-            //String cmdArgs = splitCommand(sc.nextLine());    
-       }
-        /* 
-           if (args.length > 0){
-           switch (args[0]){
-           case "ptime":
-           break;
-           case "history":
-           break;
-           case "list":
-           break;
-           case "cd":
-           break;
-           case "exit":
-           System.exit(0);
-           break;
-           default:
-        // Try to run as external command
-        break;
-           } 
-           }
-
-
-        // String[] commands = new String[args.length-1];
-        // for (String s : args){
-        //     System.out.println(s);
-        // }
-        //
-        */
+            String command = input.nextLine();
+            if (!command.isEmpty()) {
+                commandHistory.add(command);
+                runCommand(command);
+            }
+        }
     }
+
     /**
-     * Display the shell prompt
+     * Runs the specified command if valid.
      *
-     * "[currentdir]:"
+     * @param command The command to run.
      */
-    private static void prompt(){
-        System.out.println("[%s]: ",); 
+    private static void runCommand(@NotNull String command) {
+        if (!command.isEmpty()) {  // Ensure at least command name (commandArgs[0]) is included.
+            String[] commandArgs = splitCommand(command);
+            switch (commandArgs[0]) {  // Test if commandArgs[0] is a built in shell command:
+                case "ptime":               //   If so, perform that operation
+                    showProcessTime();
+                    break;
+                case "history":
+                    listHistory();
+                    break;
+                case "^":
+                    int commandNum = commandArgs.length > 1 ? Integer.valueOf(commandArgs[1]) : 0;
+                    boolean numInBounds = (0 < commandNum && commandNum <= commandHistory.size());
+
+                    if (numInBounds) {
+                        boolean willLoop = commandHistory.get(commandNum - 1).equals("^ " + commandNum);
+                        if (!willLoop) {
+                            runCommand(commandHistory.get(commandNum - 1));
+                        } else {
+                            System.out.printf("Command \"%s\" would created an infinite loop. Command not executed\n",
+                                    command);
+                        }
+                    }
+                    break;
+                case "list":
+                    listDirectory();
+                    break;
+                case "cd":
+                    String toPath = commandArgs.length > 1 ? commandArgs[1] : "~";
+                    changeDirectory(toPath);
+                    break;
+                case "exit":
+                    System.exit(0);
+                    break;
+                default:                     //   Else, attempt to execute it as an external program
+                    runAsExternal(commandArgs);
+                    break;
+            }
+
+        }
     }
 
     /**
-     * Command: ptime 
-     *
      * Display the number of seconds (4 digits past the decimal) spent executing (waiting for) child process.
+     * Command to execute: ptime
      */
-    private static void ptime(){
+    private static void showProcessTime() {
+        //TODO: "ptime": Display the number of seconds (4 digits past the decimal) spent executing (waiting for) child process.
+        System.out.println("showProcessTime function called");
     }
 
     /**
-     * Command: history
-     *
      * Show the command history
+     * Command to execute: history
      */
-    private static void history(){
-
+    private static void listHistory() {
+        System.out.println("-- Command History --");
+        for (int i = 0; i < commandHistory.size(); ++i) {
+            System.out.println((i + 1) + " : " + commandHistory.get(i));
+        }
     }
 
     /**
-     * Command: list
-     *
      * List the contents of the current directory.
+     * Command to execute: list
      */
-    private static void list(){
+    private static void listDirectory() {
+        File currentDir = new File(System.getProperty("user.dir"));
 
-    } 
+
+        for (File file : currentDir.listFiles()) {
+            printFileInfo(file);
+        }
+    }
 
     /**
-     * Command: cd
+     * Print the information of the specified file into a format similar to the "ls -l" bash command.
+     * (i.e. "[permissions for current user] [size in bytes] [date last modified] [name of file]").
      *
-     * Change the directory to specified location or to home directory. 
-     * */
-    private static void cd(String path){
-        if (path == ""){
-            // change to home directory
-        } else {
-            // check if specified path exists
+     * @param file The file to print information about.
+     */
+    private static void printFileInfo(@NotNull File file) {
+        char d = file.isDirectory() ? 'd' : '-';
+        char r = file.canRead() ? 'r' : '-';
+        char w = file.canWrite() ? 'w' : '-';
+        char x = file.canExecute() ? 'x' : '-';
+
+        Date lastModifiedDate = new Date(file.lastModified());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy HH:mm");
+
+        System.out.printf("%c%c%c%c %10s %s %s", d, r, w, x, file.length(), dateFormat.format(lastModifiedDate),
+                file.getName());
+        System.out.println();
+    }
+
+    /**
+     * Change the directory to specified location or to home directory.
+     * Command to execute: cd [path] (no path defaults to "~")
+     *
+     * @param path The path of the directory to change to.
+     */
+    private static void changeDirectory(@NotNull String path) {
+        if (path.equals("~")) {
+            // Change to home directory
+            System.setProperty("user.dir", System.getProperty("user.home"));
+        } else if (path.equals("..")) {
+            // TODO: "cd": Change to specified path of directory
+            // Check if specified path exists
             //      If it does, change to it
             //      Else, print an error message
         }
+    }
 
-        // Print current directory 
+    //TODO: "|": Pipe between two external commands.
+
+    private static void runAsExternal(@NotNull String[] commandArgs) {
+        //TODO: runAsExternal(): Run external command if valid
+
+        System.out.printf("runAsExternal called on command %s\n", commandArgs[0]);
+
     }
 
 
-    /*
+    /**
      * Split the user command by spaces, but preserving them when inside double-quotes.
-     * Code Adapted from: 
-     * https://stackoverflow.com/questions/366202/regex-for-splitting-a-string-using-space-when-not-surrounded-by-single-or-double
+     * Code Adapted from:
+     * https://stackoverflow.com/questions/366202/regex-for-splitting-a-string-using-space-when-not-surrounded-by-
+     * single-or-double
      */
-    public static String[] splitCommand(String command) {
+    @NotNull
+    private static String[] splitCommand(String command) {
         java.util.List<String> matchList = new java.util.ArrayList<>();
 
         Pattern regex = Pattern.compile("[^\\s\"']+|\"([^\"]*)\"|'([^']*)'");
